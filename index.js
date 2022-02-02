09bde5f4e987fb2e16ef44e59aabc27acc038142
Donte Ladatto <donte279@gmail.com>
2022-02-02 16:59:17 -0600

    commit tree e0c121f9742ea7540a8d2a8ba6f16ea81b9b4488

---
+++ index.js
@@ -0,0 +1,9 @@
+function introduction(name = "Donte") {
+    return `Hi, my name is ${name}.`
+}
+function introductionWithLanguage(name = "Donte", language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+function introductionWithLanguageOptional(name = "Donte", language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
